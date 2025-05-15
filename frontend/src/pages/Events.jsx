96b3df26fd934db3714a23aa212bc0356933a0c8
Jayme Desrosiers <40217128+fatal-zio@users.noreply.github.com>
2025-05-15 15:51:07 -0500

    Moved fetch logic to a loader function.

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,25 +1,10 @@
-import { Link } from 'react-router-dom';
-
-const DUMMY_EVENTS = [
-  { id: 'e1', title: 'Some event' },
-  { id: 'e2', title: 'Another event' },
-  {
-    id: 'e3',
-    title: 'Yet another',
-  },
-];
+import { useLoaderData } from 'react-router-dom';
+import EventsList from '../components/EventsList';
 
 function EventsPage() {
-  return (
-    <>
-      <h1>EventsPage</h1>
-      {DUMMY_EVENTS.map((event) => (
-        <li key={event.id}>
-          <Link to={event.id}>{event.title}</Link>
-        </li>
-      ))}
-    </>
-  );
+  const events = useLoaderData();
+
+  return <EventsList events={events} />;
 }
 
 export default EventsPage;
